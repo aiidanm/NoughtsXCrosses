a24d3964e8a0028a4540d875f5e00544d016bd5c
aiidanm <iiiaiid4niii@gmail.com>
2022-04-11 20:01:57 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import Button from './Button';
+import styled from 'styled-components'
+
+const StyledDiv = styled.div`
+  color: blue;
+`;
 
 function App() {
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <table>
+        <tbody>
+        <tr>
+          <td>
+            <StyledDiv>1</StyledDiv>
+          </td>
+          <td>
+            <div>2</div>
+          </td>
+          <td>
+            <div>3</div>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <div>1</div>
+          </td>
+          <td>
+            <div>2</div>
+          </td>
+          <td>
+            <div>3</div>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <div>1</div>
+          </td>
+          <td>
+            <div>2</div>
+          </td>
+          <td>
+            <div>3</div>
+          </td>
+        </tr>
+        </tbody>
+      </table>
     </div>
   );
 }
+
+
+
 
 export default App;
---
+++ src/Button.js
@@ -0,0 +1,17 @@
+import {useState} from 'react';
+
+function Button({ text }) {
+    const [count, setCount] = useState(0);
+    const handleClick = () => {
+        console.log("Clicked");
+        setCount(count + 1);
+        console.log(count);
+    
+      }
+
+
+return (
+    <button onClick={handleClick} className={count % 2 == 0 ? "bluebutton" : "redbutton"}>{text}</button>
+)
+}
+export default Button;
