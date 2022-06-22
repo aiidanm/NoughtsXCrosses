236668d14e2856969d9856f34b04a42e715693dc
aiidanm <iiiaiid4niii@gmail.com>
2022-06-22 23:10:45 +0100

    Update Button.js

--- src/Button.js
+++ src/Button.js
@@ -18,13 +18,7 @@ function Button({ count, setCount }) {
     const [entry, setEntry] = useState("")
     const [isDisabled, setIsdisabled] = useState(false)
     const handleClick = () => {
-        console.log(isDisabled)
-        if(isDisabled) return;
-        count % 2 == 0 ? setEntry("X") : setEntry("O")
-        console.log("Clicked");
-        setCount(count + 1);
-        console.log(count);
-        setIsdisabled(true);
+        
       }
 
 
